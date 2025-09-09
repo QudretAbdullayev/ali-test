bdbbdd7f12bbc5cb054f72a12a7d02e1125a00ac
Qudret Abdullayev <abdullayevqudret97@gmail.com>
2025-09-09 15:13:56 +0400

    ali test

---
+++ src/app/page.js
@@ -0,0 +1,66 @@
+import Image from "next/image";
+import styles from "./page.module.scss";
+
+export default function Home() {
+  return (
+    <div>
+      <div className={styles.container}>
+        Program * Container
+      </div>
+      <div className={styles.container1}>
+        Program * Container 1
+      </div>
+      <div className={styles.container2}>
+        Program * Container 2
+      </div>
+      <div className={styles.container3}>
+        Program * Container 3
+      </div>
+      <div className={styles.container4}>
+        Program * Container 4
+      </div>
+      <div className={styles.container5}>
+        Program * Container 5
+      </div>
+      <div className={styles.container6}>
+        Program * Container 6
+      </div>
+      <div className={styles.container7}>
+        Program * Container 7
+      </div>
+      <div className={styles.container8}>
+        Program * Container 8
+      </div>
+      <div className={styles.container9}>
+        Program * Container 9
+      </div>
+      <div className={styles.container10}>
+        Program * Container 10
+      </div>
+      <div className={styles.container11}>
+        Program * Container 11
+      </div>
+      <div className={styles.container12}>
+        Program * Container 12
+      </div>
+      <div className={styles.container13}>
+        Program * Container 13
+      </div>
+      <div className={styles.container14}>
+        Program * Container 14
+      </div>
+      <div className={styles.container15}>
+        Program * Container 15
+      </div>
+      <div className={styles.container16}>
+        Program * Container 16
+      </div>
+      <div className={styles.container17}>
+        Program * Container 17
+      </div>
+      <div className={styles.container18}>
+        Program * Container 18
+      </div>
+    </div>
+  );
+}
